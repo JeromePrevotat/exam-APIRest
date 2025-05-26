f59ae770b1b46ba82027c7d087f204e1b2089f31
JeromePrevotat <jer.prevotat@gmail.com>
2025-05-26 10:10:56 +0200

    TaskDao

--- exam-apirest/src/main/java/com/humanbooster/dao/TaskDao.java
+++ exam-apirest/src/main/java/com/humanbooster/dao/TaskDao.java
@@ -1,5 +1,14 @@
 package com.humanbooster.dao;
 
-public class TaskDao {
+import org.hibernate.SessionFactory;
+
+import com.humanbooster.model.Task;
+
+public class TaskDao extends GenericDaoImp<Task, Integer>{
+
+    public TaskDao(SessionFactory sessionFactory) {
+        super(sessionFactory, Task.class);
+    }
+
 
 }
