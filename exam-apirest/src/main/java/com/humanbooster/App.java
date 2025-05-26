6b424a8d140017b6f9e9dfd2c7b5622cd250c24f
JeromePrevotat <jer.prevotat@gmail.com>
2025-05-26 10:27:51 +0200

    Main : runDB + runServlet

--- exam-apirest/src/main/java/com/humanbooster/App.java
+++ exam-apirest/src/main/java/com/humanbooster/App.java
@@ -1,7 +1,45 @@
 package com.humanbooster;
 
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.servlet.ServletContainer;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import com.humanbooster.apirest.ApiApplication;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            runDB();
+            runServlet();
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    private static void runDB(){
+        System.out.println("Démarrage de l'application");
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+            .configure()
+            .build();
+        Metadata metadata = new MetadataSources(registry).buildMetadata();
+        SessionFactory sessionFactory = metadata.buildSessionFactory();
+        System.out.println("Connexion réussie !");
+    }
+
+    private static void runServlet() throws Exception {
+        ResourceConfig config = new ApiApplication();
+        ServletHolder servlet = new ServletHolder(new ServletContainer(config));
+        Server server = new Server(8080);
+        ServletContextHandler context = new ServletContextHandler(server, "/");
+        context.addServlet(servlet, "/*");
+        server.start();
+        server.join();
     }
 }
