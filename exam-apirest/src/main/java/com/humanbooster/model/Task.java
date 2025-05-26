ad74f70d9c8b01fa4d795e0868bfaf6bf2a61605
JeromePrevotat <jer.prevotat@gmail.com>
2025-05-26 10:06:24 +0200

    Class Task

--- exam-apirest/src/main/java/com/humanbooster/model/Task.java
+++ exam-apirest/src/main/java/com/humanbooster/model/Task.java
@@ -1,5 +1,106 @@
 package com.humanbooster.model;
 
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
 public class Task {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private int id;
+
+    @NotBlank
+    private String title;
+
+    private String description;
+    
+    private boolean done = false;
+
+    @JsonIgnore
+    private LocalDate createdAt;
+    @JsonIgnore
+    private LocalDate updatedAt;
+
+    public Task(){}
+    
+    public Task(String title, String description, boolean done, LocalDate createdAt){
+        this.title = title;
+        this.description = description;
+        this.done = false;
+        this.createdAt = createdAt;
+    }
+
+    // GETTER
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public LocalDate getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDate getUpdatedAt() {
+        return updatedAt;
+    }
+
+    // SETTER
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    public void setCreatedAt(LocalDate createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public void setUpdatedAt(LocalDate updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    // METHODS
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Task{");
+        sb.append("id=").append(id);
+        sb.append(", title=").append(title);
+        sb.append(", description=").append(description);
+        sb.append(", done=").append(done);
+        sb.append(", createdAt=").append(createdAt);
+        sb.append(", updatedAt=").append(updatedAt);
+        sb.append('}');
+        return sb.toString();
+    }
 
 }
